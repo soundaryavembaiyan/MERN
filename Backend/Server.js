b7965779ce669eae93e8cb5d7e843c234bfd4646
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2025-10-19 12:51:29 +0530

    js and db added

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,12 +1,30 @@
-// import express from 'express'
-
+// run - npm start, npm i -g nodemon
 const express = require('express');
+require('dotenv').config(); //express
+
+const mongoose = require("mongoose"); //connect mongoose
+
 const app = express();
+const taskRoutes = require("./routes/taskRoute");
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//Middleware
+app.get('/', (req, res, next) => {
+  console.log("path" + req.path + "method" + req.method)
+  next()
 })
 
-app.listen(4000,()=>{
-    console.log("listening to 4000")
-})
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World') //visual o/p
+// })
+
+//DB connection
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("DB connected successfully & listening to " + process.env.PORT) //port listening
+  })
+}).catch((error)=>console.log(error)) //
+
+app.use("/api/tasks", taskRoutes)
+
---
+++ Backend/routes/taskRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { createTask } = require("../controllers/taskController");
+router.post("/", createTask);
+
+module.exports = router;

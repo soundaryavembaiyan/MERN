5fe7f6fa7e90ae437dde7a5b014900dbbb15f53d
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2025-11-01 22:08:24 +0530

    creation

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+// const { default: mongoose } = require("mongoose");
 const taskModel = require("../models/TaskModel");
 
 // To create a Task - POST
@@ -12,4 +14,67 @@ const createTask = async (req, res) => {
     }
 };
 
-module.exports = { createTask };
+// To get all tasks
+const getTasks = async (req, res) => {
+    try {
+        const task = await taskModel.find({});
+        res.status(200).json(task);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+}
+
+
+// To get only one tasks
+const getSingleTask = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return req.status(404).json({ error: 'Task Not Found!' })
+    }
+    try {
+        const singleTask = await taskModel.findById(id);
+        res.status(200).json(singleTask);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+}
+
+// To update a task - PATCH
+const updateTask = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return req.status(404).json({ error: 'Task Not Updated!' })
+    }
+
+    try {
+        const task = await taskModel.findByIdAndUpdate({
+            _id: id
+        },
+            {
+                ...req.body
+            });
+        res.status(200).json(task);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+}
+
+// To delete a task - DELETE
+const deleteTask = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return req.status(404).json({ error: 'Task Not Deleted!' })
+    }
+
+
+    try {
+        const task = await taskModel.findByIdAndDelete(id);
+        res.status(200).json(task);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+
+}
+
+
+module.exports = { createTask, getTasks, getSingleTask, updateTask, deleteTask };
--- Backend/routes/taskRoute.js
+++ Backend/routes/taskRoute.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createTask } = require("../controllers/taskController");
-router.post("/", createTask);
+const { createTask, getTasks, getSingleTask, updateTask, deleteTask } = require("../controllers/taskController");
+
+router.post("/", createTask); //add task
+router.get("/", getTasks); // get all tasks
+router.get("/:id", getSingleTask); // get single task
+router.patch("/:id", updateTask); // update task
+router.delete("/:id", deleteTask); // delete task
 
 module.exports = router;
